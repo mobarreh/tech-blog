3c0321ef017276543b118fa765fc0ef14d2d6791
shtuupid <mohamedbarreh8@gmail.com>
2023-06-03 08:05:29 +0100

    seeds

---
+++ seeds/index.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models');
+
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+    });
+  }
+
+  const comments = await Comment.bulkCreate(commentData, {
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
